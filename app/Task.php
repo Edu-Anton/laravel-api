8d0b1ceedc2f633b820dac1965cc0bd418f227d7
Edu-Anton <eduardo1acc@gmail.com>
2020-04-26 15:26:25 -0500

    basic laravel crud

---
+++ app/Task.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'task', 'description'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
+}
